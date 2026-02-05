1dfb031c7f84fe823a3733734f6b517ee5a83207
quangfoden <nguyenvanquang22103@gmail.com>
2026-02-05 23:38:22 +0700

    up

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CardController;
-use App\Http\Controllers\Api\TemplateController;
+use App\Http\Controllers\API\TemplateController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
